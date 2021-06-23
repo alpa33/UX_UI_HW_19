d407175cd7eabbc567429742cd571f5a0bb7adc4
alpa33 <77124066+alpa33@users.noreply.github.com>
2021-06-23 14:41:03 -0500

    button attempt number 2

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 console.log("your index.js file is loaded correctly");
-$("#Button2").click(function() {
+
+$("#Button2").click(function(){            
     $('html, body').animate({
-        scrollTop: $("#Hero").offset().top
-    }, 800);
+            scrollTop: $("#Hero").offset().top
+        }, 500);
 });
