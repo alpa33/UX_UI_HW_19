1c49bdaceb5273840baa6888c5d89845d6cbe8d9
alpa33 <77124066+alpa33@users.noreply.github.com>
2021-06-23 15:52:58 -0500

    smoothscroll attempt

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,4 @@
 console.log("your index.js file is loaded correctly");
-$("#Button2").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".foster").offset().top
-    }, 2000);
-});
+$( "#Button2" ).click(function() {
+    $( "#foster" ).click();
+  });
