b5a498b76cf3e08c0fffb95563113d2d5a1223fb
alpa33 <77124066+alpa33@users.noreply.github.com>
2021-06-23 14:30:32 -0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,6 @@
 console.log("your index.js file is loaded correctly");
+$("#Button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Hero").offset().top
+    }, 800);
+});
