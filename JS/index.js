c932ac47f26ee26abfe9cac4bb47a745993eed45
alpa33 <77124066+alpa33@users.noreply.github.com>
2021-06-23 16:24:47 -0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 console.log("your index.js file is loaded correctly");
 
-$( "#Button2" ).trigger("scroll",function() {
+jQuery( "#Button2" ).trigger("scroll",function() {
 console.log("You clicked this");
   });
   
