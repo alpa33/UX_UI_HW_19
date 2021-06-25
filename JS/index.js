c3ad0afb7831a7b3cefa99a9c6e27cb67ca9fc1c
alpa33 <77124066+alpa33@users.noreply.github.com>
2021-06-25 15:52:48 -0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,7 @@
 console.log("your index.js file is loaded correctly");
-
+$( "#Child4" ).click(function() {
+	alert( "Make a one-time donation to Thundering Paws?" );
+  });
 
 $("#Button2").trigger("scroll",function() {
 	console.log("You clicked this");
